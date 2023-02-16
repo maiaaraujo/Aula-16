5787cde888b275ae81e5b38bf2a6a3c74345729b
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-16 11:55:33 -0300

    lista2

--- Lista/scripts.js
+++ Lista/scripts.js
@@ -2,12 +2,28 @@ let produtos = [];
 
 function adicionarItem(){
     produtos.push(item.value);
+
     lista.innerHTML = "";
 
-    for (let i = 0; i < produtos.length; i++){
-       lista.innerHTML += `<li class="list-group-item">${produtos[i]}</li>`;
-    }
+   
+    localStorage.setItem("produtos", JSON.stringify(produtos))
+
+    listarItens();     
 }
+
+function listarItens(){
+    let itens = JSON.parse(localStorage.getItem("produtos"));
+    lista.innerHTML = "";
+
+    if (itens && itens.length > 0)
+        for (let i = 0; i < itens.length; i++){
+            lista.innerHTML += `<li class="list-group-item">${itens[i]}</li>`;
+     }
+
+listarItens();     
+    
+}
+
 
 
 
