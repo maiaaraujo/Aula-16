931ab4c765dca48a6b745599cbe73d019cb1c4ca
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-16 08:59:41 -0300

    aula-16

---
+++ Exercícios/eventos.js
@@ -0,0 +1,7 @@
+let n = 1;
+
+while (n <= 100) {
+
+    document.write (n + "<br>")
+    n = n + 1;
+}
---
+++ música/eventos.js
@@ -0,0 +1,23 @@
+
+
+//inicio
+let numero = 1;
+
+while (numero <= 10) { //condição de término
+    
+    document.write("Ola mundo <br>");
+    
+    numero = numero +1; // passo / meio de transporte / incremento/decremento
+}
+
+document.write("<hr>");
+
+let n = 1;
+
+while (n <= 5) {
+    document.write("<br> Agora na velocidade: " + n);
+
+    document.write("<br> Creu Creu Creu Creu Creu <br>");
+
+    n = n + 1;
+}
